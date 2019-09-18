ec5eadffd9397a3cb7ed2760a8c656c11952e28e
xiao pengwei <xpwi@qq.com>
2019-09-18 15:33:13 +0800

    20190918

---
+++ java-algorithm/src/main/java/com/xiaopengwei/algorithm/commontest/Combination.java
@@ -0,0 +1,11 @@
+package com.xiaopengwei.algorithm.commontest;
+
+/**
+ * <p>
+ * 组合
+ *
+ * @author xpwi
+ * @since 2019-09-17
+ */
+public class Combination {
+}
--- java-algorithm/src/main/java/com/xiaopengwei/algorithm/commontest/MaxSubStringSum.java
+++ java-algorithm/src/main/java/com/xiaopengwei/algorithm/commontest/MaxSubStringSum.java
@@ -1,32 +1,34 @@
-package com.xiaopengwei.algorithm;
+package com.xiaopengwei.algorithm.commontest;
 
 /**
  * <p>
+ * 最大连续连续子序列和
  *
  * @author xpwi
  * @since 2019-09-17
  */
 public class MaxSubStringSum {
     public static void main(String[] args) {
-        int[] a = { -2, 4, -3, 5, 7, -1, 8, 1 };
+        int[] a = {-2, 4, -3, 5, 7, -1, 8, 1};
         int max = maxSubSum1(a);
         System.out.println(max);
-        // 21
     }
 
     private static int maxSubSum1(int[] a) {
         int maxSum = 0;
+
         for (int i = 0; i < a.length; i++) {
+            int thisSum = 0;
+
             for (int j = i; j < a.length; j++) {
-                int thisSum = 0;
-                for (int k = i; k <= j; k++) {
-                    thisSum += a[k];
-                }
-                if (thisSum > maxSum) {
-                    maxSum = thisSum;
-                }
+                thisSum += a[j];
+            }
+            if (thisSum > maxSum) {
+                maxSum = thisSum;
             }
         }
+
+
         return maxSum;
     }
 }
